eaa3d8b6a8f56f6901fe5d619e783637b5654677
Billy Mulaji <b2mutombo2011@gmail.com>
2017-07-29 20:14:19 +0200

    links

---
+++ DefinitionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DefinitionViewController.swift
+//  Mojidictionary
+//
+//  Created by Billy Lotus on 2017/07/29.
+//  Copyright © 2017 Billy Lotus. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    var emoji = "NO OONE"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(emoji)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
